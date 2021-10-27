7f2877fc004850df551700eb29af07ac6a51be3e
Bruno <brunobold54@outlook.com>
2021-10-27 18:29:46 +0100

    Added Lists

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <Grid container spacing={4} justify="center">
+        <Grid item xs={12} md={6}>
+          <ul className="networking">
+            <li>PiHole</li>
+            <li>PiAlert</li>
+          </ul>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <p>Hello World</p>
+        </Grid>
+      </Grid>
     </div>
   );
 }
