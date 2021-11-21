c7d18041594e466afada6a456bcfeec009962477
Bruno <brunobold54@outlook.com>
2021-11-21 03:14:56 +0000

    Formatting Change
    Added headers, changed formatting and layout of lists and headers.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
+
+import { Grid, Typography, Box } from '@mui/material';
 
 import './App.css';
+
+let name = "Bruno"
 
 function App() {
   return (
     <div className="App">
-      <Grid container spacing={4} justify="center">
-        <Grid item xs={12} md={6}>
-          <ul className="networking">
-            <li>PiHole</li>
-            <li>PfSense</li>
-          </ul>
+
+      <Box mt={15} ml={10}>
+        <Grid container spacing={2} justify="left" >
+          <Typography variant="h3">Good Afternoon, <b>{name}!</b></Typography>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <ul className="monitoring">
-            <li>PiAlert</li>
-            <li>NetData</li>
-            <li>SmokePing</li>
-            <li>Portainer</li>
-          </ul>
+      </Box>
+
+      <Box mt={5} ml={15}>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h4"><b>Networking</b></Typography>
+            <ul className="networking">
+              <li><b>PIHOLE</b></li>
+              <li>PfSense</li>
+            </ul>
+          </Grid>
+          <Grid item xs={12} md={6}>
+          <Typography variant="h4"><b>Monitoring</b></Typography>
+            <ul className="monitoring">
+              <li>PiAlert</li>
+              <li>NetData</li>
+              <li>SmokePing</li>
+              <li>Portainer</li>
+            </ul>
+          </Grid>
+          <Grid item xs={12} md={6}>
+          <Typography variant="h4"><b>Uncategorized</b></Typography>
+            <ul className="uncategorized">
+              <li>Pterodactyl</li>
+              <li>Jellyfin</li>
+            </ul>
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={6}>
-          <ul className="uncategorized">
-            <li>Pterodactyl</li>
-            <li>Jellyfin</li>
-          </ul>
-        </Grid>
-      </Grid>
+      </Box>
     </div>
   );
 }
